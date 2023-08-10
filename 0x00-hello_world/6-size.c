a49177f032e87440152a3ea0c4b1a9874c185802
Lloyd <llorugwe@gmail.com>
2023-08-10 16:53:33 -0700

    Size is not grandeur, and territory does not make a nation

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,10 +8,10 @@
 
 int main(void)
 {
-	print("Size of a char: %lu byte(s)", sizeof(char));
-	print("Size of an int: %lu byte(s)", sizeof(int));
-	print("Size of a long int: %lu byte(s)", sizeof(long int));
-	print("Size of a long long int: %lu byte(s)", sizeof(long long int));
-	print("Size of a float: %lu byte(s)", sizeof(float));
+	print("Size of a char: %d byte(s)\n", sizeof(char));
+	print("Size of an int: %d byte(s)\n", sizeof(int));
+	print("Size of a long int: %d byte(s)\n", sizeof(long int));
+	print("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+	print("Size of a float: %d byte(s)\n", sizeof(float));
 	return (0);
 }
