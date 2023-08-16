5d385fa634bb18cc996b53dc9c8012bb0d73e96a
Lloyd <llorugwe@gmail.com>
2023-08-16 16:18:31 -0700

    0x03. C - Debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* main - This is a test function for positive or negative
+* Return: will return 0 upon success
+*/
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - will causes an infinite loop to occur
+ * Return: returns 0 when successful
+ */
+
+int main(void)
+{
+	int i;
+
+	printf("Infinite loop incoming :(\n");
+
+	i = 0;
+	/**
+	*while (i < 10)
+	*{
+	*	putchar(i);
+	*}
+	*/
+	printf("infinity loop avoided! \\o/\n");
+
+	return (0);
+}
